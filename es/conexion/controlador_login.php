d06514c694a1d322e5a6763c661d22dd751fb099
sponkurtus2 <sponnkurtus3@gmail.com>
2024-09-26 22:39:56 -0600

    Added cookies

--- es/conexion/controlador_login.php
+++ es/conexion/controlador_login.php
@@ -5,13 +5,19 @@ if (!empty($_POST["btningresar"])) {
     if (!empty($_POST["usuario"]) and !empty($_POST["password"])) {
         $usuario = $_POST["usuario"];
         $password = $_POST["password"];
-        $password = MD5($password);
+        // $password = MD5($password);
         $sql = $conn->query("select * from empleados where usr = '$usuario' and passwd = '$password'");
         if ($datos = $sql->fetch_object()) {
             $_SESSION["id"]=$datos->idEmple;
             $_SESSION["nombre"]=$datos->nomEmple;
             $_SESSION["apellido"]=$datos->apeEmple;
             $_SESSION["admin"]=$datos->isadmin;
+
+            // We save the cookies here 👇
+            setcookie("usuario", $usuario, time() + (86400 * 30), "/"); // 1 day duration
+            setcookie("nombre", $datos->nomEmple, time() + (86400 * 30), "/");
+            setcookie("apellido", $datos->apeEmple, time() + (86400 * 30), "/");
+
             header("location: ./administracion.php");
         }
         else {
@@ -21,7 +27,7 @@ if (!empty($_POST["btningresar"])) {
     } else {
         echo "<div class='alert alert-warning'>Campos vacíos</div>";
     }
-    
+
 }
 
 ?>
