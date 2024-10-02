1acbd69a31e22e915ca0b36d514df46f43d39c3f
sponkurtus2 <sponnkurtus3@gmail.com>
2024-10-02 14:09:47 -0600

    Fixed register

--- es/TEMPLATE/head.php
+++ es/TEMPLATE/head.php
@@ -14,11 +14,12 @@ if (empty($title)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-        </script>
+            integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
     <title><?php echo $title ?></title>
-    <link rel="icon" href="<?php echo $whereami ?>./adobe/leonlogonuevo-01.png">
+    <link rel="icon" href="<?php echo $whereami; ?>.adobe/leonlogonuevo-01.png">
+
 
     <style>
